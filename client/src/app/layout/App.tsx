589696613ed0ec0d0d20536a76a3d7d807608fab
Sibongiseni <sibongiseni.mxinwa@gmail.com>
2025-06-24 16:53:51 +0200

    Adding Sorting, Filtering, Searching and Pagination functionality

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,7 +6,7 @@ import {
   ThemeProvider,
 } from '@mui/material';
 import NavBar from './NavBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { useAppSelector } from '../store/store';
 
 function App() {
@@ -23,8 +23,9 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ScrollRestoration />
       <CssBaseline />
-      <NavBar/>
+      <NavBar />
       <Box
         sx={{
           minHeight: '100vh',
