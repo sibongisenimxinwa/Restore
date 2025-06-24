102cb58737d8ad2a59b54336a4b6bd0417aa1c39
Sibongiseni <sibongiseni.mxinwa@gmail.com>
2025-06-24 11:19:08 +0200

    Adding the shopping cart feature

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:3000");
 });
 
 app.MapControllers();
--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -7,6 +7,8 @@ import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
 import NotFound from '../errors/NotFound';
 import ServerError from '../errors/ServerError';
+import BasketPage from '../../features/basket/BasketPage';
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 
 export const router = createBrowserRouter([
   {
@@ -18,10 +20,11 @@ export const router = createBrowserRouter([
       { path: '/catalog/:id', element: <ProductDetails /> },
       { path: '/about', element: <AboutPage /> },
       { path: '/contact', element: <ContactPage /> },
-      {path: '/server-error', element: <ServerError />},
-            {path: '/not-found', element: <NotFound />},
-            {path: '*', element: <Navigate replace to='/not-found' />}
-
+      { path: '/basket', element: <BasketPage /> },
+      { path: '/checkout', element: <CheckoutPage /> },
+      { path: '/server-error', element: <ServerError /> },
+      { path: '/not-found', element: <NotFound /> },
+      { path: '*', element: <Navigate replace to='/not-found' /> },
     ],
   },
 ]);
